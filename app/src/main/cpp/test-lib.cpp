c8dc567d97b2143cbce27d4a33e0628d70abdfd2
Solo_one <quanshishan@qq.com>
2018-05-26 12:48:29 +0800

    update

---
+++ app/src/main/cpp/test-lib.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Solo_one on 2018/5/26.
+//
+#include <jni.h>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_xiaoming_testjni_JniUtil_test(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+}
---
+++ app/src/main/java/com/xiaoming/testjni/JniUtil.java
@@ -0,0 +1,10 @@
+package com.xiaoming.testjni;
+
+/**
+ * @author xiaoming
+ * @version 1.0.0
+ */
+public class JniUtil {
+
+    public native void test();
+}
